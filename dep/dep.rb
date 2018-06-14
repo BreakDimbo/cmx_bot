f450d10f14161369696177c9840a41587a044ab0
董昊 <dhpigtc@gmail.com>
2018-06-14 10:01:28 +0800

    dep.rb add fbot

--- dep/dep.rb
+++ dep/dep.rb
@@ -1,15 +1,15 @@
 #!/Users/break/.rvm/rubies/ruby-2.4.2/bin/ruby
 
-# usage ./mk_push_deploy.rb -s[service] -t[target] -d[work_directory]
-# example mk_push_deploy.rb -s tower,msg_pusher -t udesk.test.dcc,udesk.cti.sipp -c docker -d /Users/break/Work/Workspace/udesk/udesk_qilin_cti
-# use default config: mk_push_deploy.rb -s tower,msg_pusher -t udesk.test.dcc,udesk.cti.sipp
+# usage ./dep.rb -s[service] -t[target] -d[work_directory]
+# example dep.rb -s bot,fbot -t break.bwh.com -d /Users/break/Work/Workspace/udesk/udesk_qilin_cti
+# use default config: dep.rb -s bot,fbot -t break.bwh.com
 require 'optparse'
 
 DefaultDir = "/Users/break/Documents/Geek/cmx_bot"
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: ./mk_push_deploy.rb -s[services] -t[targets] -d[work_directory]"
+  opts.banner = "Usage: ./dep.rb -s[services] -t[targets] -d[work_directory]"
 
   opts.on("-s", "--services [Services]", Array, "make services") do |v|
     options[:services] = v
@@ -29,10 +29,13 @@ p options
 def pull_and_compile_restart(services, target, dir)
   services.each do |service|
     puts "start pull #{service} to #{target}"
-    raise unless system("ssh #{target} \"cd /home/break/documents/cmx_bot; export GOPATH=/home/break/documents/cmx_bot; git pull; /usr/local/go/bin/go build -o bin/bot bot/ai_x\"")
-    raise unless system("ssh #{target} \"sudo systemctl stop cmxbot.service\"")
-    raise unless system("ssh #{target} \"cp /home/break/documents/cmx_bot/bin/bot /usr/local/cmx_bot/current/bin\"")
-    raise unless system("ssh #{target} \"sudo systemctl start cmxbot.service\"")
+    mkdir = "ai_x" if service == "bot"
+    mkdir = "firebot" if service == "fbot"
+
+    raise unless system("ssh #{target} \"cd /home/break/documents/cmx_bot; export GOPATH=/home/break/documents/cmx_bot; git pull; /usr/local/go/bin/go build -o bin/#{service} bot/#{mkdir}\"")
+    raise unless system("ssh #{target} \"sudo systemctl stop #{service}.service\"")
+    raise unless system("ssh #{target} \"cp /home/break/documents/cmx_bot/bin/#{service} /usr/local/cmx_bot/current/bin\"")
+    raise unless system("ssh #{target} \"sudo systemctl start #{service}.service\"")
     puts "over restart #{service} on #{target}"
   end
 end
