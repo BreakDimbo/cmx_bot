69f212febb14fa0aff2bf5e55f543c2da49dbda7
董昊 <dhpigtc@gmail.com>
2018-06-25 23:42:34 +0800

    fix

--- src/bot/firebot/bot/handler.go
+++ src/bot/firebot/bot/handler.go
@@ -29,7 +29,7 @@ func HandleNotification(e *gomastodon.NotificationEvent) {
 			tootToPost = firstContent[index+12:]
 			lastIndex := strings.Index(tootToPost, "条\n")
 			tootToPost = fmt.Sprintf("%s:%s。", "咳咳...注意！昨天最活跃（话唠）县民是", tootToPost[:lastIndex+3])
-		} else if strings.Contains(firstContent, "#树洞") {
+		} else if strings.Contains(firstContent, "#树洞") || strings.Contains(filter(toot.SpoilerText), "#树洞") {
 			tootToPost = firstContent
 		} else {
 			reg := regexp.MustCompile("^@[^ ]*")
