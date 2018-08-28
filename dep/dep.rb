4a2e14ee0dd4440935d03f612ebbef5182a1e583
break.D <dhpigtc@gmail.com>
2018-08-28 22:41:59 +0800

    add systemd.service & update dep

--- dep/dep.rb
+++ dep/dep.rb
@@ -32,6 +32,7 @@ def pull_and_compile_restart(services, target, dir)
     mkdir = "intelbot" if service == "bot"
     mkdir = "firebot" if service == "fbot"
     mkdir = "hbot" if service == "hbot"
+    mkdir = "wikibot" if service == "wbot"
 
     raise unless system("ssh #{target} \"cd /home/break/documents/cmx_bot; export GOPATH=/home/break/documents/cmx_bot; git pull; /usr/local/go/bin/go build -o bin/#{service} bot/#{mkdir}\"")
     raise unless system("ssh #{target} \"sudo systemctl stop #{service}.service\"")
