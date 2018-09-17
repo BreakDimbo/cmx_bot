c8a8d316bafad6140c751ede0110e425fdaf5ae3
break.D <dhpigtc@gmail.com>
2018-09-17 18:54:41 +0800

    theater add block and unblock with one message

--- src/bot/theater/bot/bot.go
+++ src/bot/theater/bot/bot.go
@@ -107,11 +107,13 @@ func (a *Actor) handleNotification(ntf *gomastodon.NotificationEvent, actors map
 		switch a.Name {
 		case cons.Kurisu:
 			// if the toot is for kurisu and on public then kurisu will reply he(she) on public line
-			reply := selectReply(cons.Kurisu)
-			toot := fmt.Sprintf("@%s %s", n.Account.Username, reply)
-			_, err := a.client.Post(toot)
-			if err != nil {
-				log.SLogger.Errorf("kurisu reply to error %v", err)
+			if n.Status.Visibility == "public" {
+				reply := selectReply(cons.Kurisu)
+				toot := fmt.Sprintf("@%s %s", n.Account.Username, reply)
+				_, err := a.client.Post(toot)
+				if err != nil {
+					log.SLogger.Errorf("kurisu reply to error %v", err)
+				}
 			}
 		case cons.Okabe:
 			for _, actor := range actors {
