6ed29c126aa425cdba191b42e300f3a2f8bfbdc3
break.D <dhpigtc@gmail.com>
2019-01-10 10:35:55 +0800

    fix

--- src/bot/intelbot/bot/analyzer.go
+++ src/bot/intelbot/bot/analyzer.go
@@ -340,6 +340,7 @@ func drawChart(avatars []kvPair) string {
 	for i := range sbc.Bars {
 		sbc.Bars[i].Value = float64(avatars[i].count)
 		name := strings.Replace(avatars[i].key, ":cmx_elph:", "", -1)
+		name = strings.Replace(avatars[i].key, ":doughnut:", "", -1)
 		sbc.Bars[i].Label = validLengthFilter(name, 12)
 	}
 
